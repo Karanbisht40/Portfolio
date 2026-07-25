d31784cdea39ab9089bc2f0702d5f05b9b5ed212
Karan Bisht <karanbisht4041@gmail.com>
2026-07-25 10:44:46 +0530

    f

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,6 +32,7 @@ const Hero = () => {
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
+        {/* new? */}
         {/* Radial gradient for the container to give a faded look */}
         <div
           // chnage the bg to bg-black-100, so it matches the bg color and will blend in
